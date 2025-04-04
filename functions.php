2c10795af7a0f701f6894ea855e41402771441c5
Alexander Busnitsky <abusnitsky@gmail.com>
2025-04-04 15:46:48 +0200

    add more basic functions

--- functions.php
+++ functions.php
@@ -1,23 +1,71 @@
 <?php
 
-function getCurrentDate() {
+function getCurrentDate()
+{
     return date('Y-m-d');
 }
 
-function getNextMonthDate($date) {
+function getCurrentYear()
+{
+    return date('Y');
+}
+function getCurrentMonth()
+{
+    return date('m');
+}
+
+function getCurrentDay()
+{
+    return date('d');
+}
+
+function getCurrentMonthName()
+{
+    return date('F');
+}
+
+function getDaysInMonth($month, $year)
+{
+    return cal_days_in_month(CAL_GREGORIAN, $month, $year);
+}
+
+function getFirstDateOfMonth($month, $year)
+{
+    return "$year-$month-01";
+}
+
+function getLastDateOfMonth($month, $year)
+{
+    $daysInMonth = getDaysInMonth($month, $year);
+    return "$year-$month-$daysInMonth";
+}
+
+function getFirstDayOfWeek($month, $year)
+{
+    return (date('w', strtotime("$year-$month-01")) + 6) % 7;
+}
+
+function getLastDayOfWeek($month, $year)
+{
+    $daysInMonth = getDaysInMonth($month, $year);
+    return (date('w', strtotime("$year-$month-$daysInMonth")) + 6) % 7;
+}
+
+function getNextMonthDate($date)
+{
     $dateTime = new DateTime($date);
     $dateTime->modify('+1 month');
     return $dateTime->format('Y-m-d');
 }
 
-function getPreviousMonthDate($date) {
+function getPreviousMonthDate($date)
+{
     $dateTime = new DateTime($date);
     $dateTime->modify('-1 month');
     return $dateTime->format('Y-m-d');
 }
 
-function getCurrentTime() {
+function getCurrentTime()
+{
     return date('H:i');
 }
-
-
