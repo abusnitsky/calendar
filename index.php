2ba264f7b5ec78baf4eb3c20ac6e0d9542e32917
Alexander Busnitsky <abusnitsky@gmail.com>
2025-04-04 14:03:03 +0200

    add blank header, footer and tailwind cdn

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    require 'views/header.php';
+
+    require 'views/footer.php';
---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./pages/**/*.{html,js}', './components/**/*.{html,js}'];
