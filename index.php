4d2fc864b812bce1b8f97e1636711e67acb8055d
Alexander Busnitsky <abusnitsky@gmail.com>
2025-04-05 23:23:36 +0200

    add basic router

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 require_once 'controllers/CalendarController.php';
+require_once 'controllers/DayController.php';
 
-$controller = new CalendarController();
-$controller->handle();
+$route = $_GET['route'] ?? 'calendar';
+
+switch ($route) {
+    case 'calendar':
+        $controller = new CalendarController();
+        echo $controller->handle();
+        break;
+
+    case 'day':
+        $date = $_GET['date'] ?? null;
+        $controller = new DayController();
+        echo $controller->handle($date);
+        break;
+
+    default:
+        http_response_code(404);
+        echo '404 Not Found';
+        break;
+}
